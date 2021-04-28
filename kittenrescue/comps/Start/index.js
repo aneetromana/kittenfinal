516e8a5e9328fb25051a0d2c997f8e90082f6682
aneetromana <aneetromana167@gmail.com>
2021-04-28 03:27:53 -0700

    hi

--- kittenrescue/comps/Start/index.js
+++ kittenrescue/comps/Start/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
 
 
+const StartInput = styled.button`
+background-color:#FFD7F1;
+border-radius:  5px;
+font-size:30px;
+color: white;
+`;
 const Start = () => {
     return <div>
-        <button style={{
+        {/*<button style={{
        backgroundColor:"#FFD7F1",
        color:"#FFF"
-        }}> Start</button>
-        
+        }}> Start</button>*/}
+        <StartInput>
+            Start
+        </StartInput>
     </div>
 }
 
