07f30b7d555107cd0bb4f110d7c3fd2662802086
aneetromana <aneetromana167@gmail.com>
2021-05-16 02:04:02 -0700

    changes

--- kittenrescue/comps/Menu/index.js
+++ kittenrescue/comps/Menu/index.js
@@ -1,13 +1,16 @@
 import styled from 'styled-components';
 import Button1 from '../Button1';
+import {GiHamburgerMenu} from 'react-icons/ri'
+import React, {useState} from 'react';
 
 const MenuCont = styled.div`
 `
 const Menu = ({
 
 }) => {
+    const [open, setOpen]= useState(false);
     return <MenuCont>
-
+<GiHamburgerMenu></GiHamburgerMenu>
     </MenuCont>
 }
 
