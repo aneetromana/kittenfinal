4dbdf9ee63f9309a1640ac8eb94c766b5e7e0a98
aneetromana <aneetromana167@gmail.com>
2021-05-16 20:41:18 -0700

    message

--- kittenrescue/comps/Menu/index.js
+++ kittenrescue/comps/Menu/index.js
@@ -1,21 +1,39 @@
 import styled from 'styled-components';
 import Button from '../Menubutton';
-import {FiMenu} from 'react-icons/fi';
+import {FiMenu, FiVolumeX} from 'react-icons/fi';
 import React, {useState} from 'react';
 
 const MenuCont = styled.div`
 `
 const MenuIcon = styled.div``;
-const MenuItems = styled.div``;
+
+const MenuItems = styled.div`
+display:flex;
+flex-direction:column;
+max-width:${props=>props.width}px;
+max-height:${props=>props.width}pxpx;
+overflow:hidden;
+transition:max-width 0.5s, max-height:0.5s;
+`;
+
 const Menu = ({
 
 }) => {
-    const [open, setOpen] = useState(false);
+    const [open, setOpen] = useState(true);
+    var width = 0, height= 0;
+    if(open){
+        width=120;
+        height=100;
+    }
     return <MenuCont>
-       <MenuIcon>
+       <MenuIcon onClick={()=>setOpen(!open)}>
        <FiMenu/>
        </MenuIcon>
-        <MenuItems>
+        <MenuItems width={width} height={height}>
+        <Button
+        text="open me"
+        bgcolor="blue" 
+        />
         <Button
         text="open me"
         bgcolor="blue"
